e269ff07c96a708fa92db005cd90eb331a0a2fe2
Stephen Green <stephengreen1028@gmail.com>
2020-01-18 16:07:35 -0700

    Created public, data and routing directories.

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-// Require in the necessary dependencies 
-var bodyParser = require('body-parser')
-var express = require('express')
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+// Require in the necessary dependencies and set variables and define PORT
+// =================================================
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+// Parse application JSON
+// =================================================
 app.use(bodyParser.json())
- 
+
 app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('you posted:\n')
+    res.end(JSON.stringify(req.body, null, 2))
+});
+
+// Listen 
+// =================================================
+app.listen(PORT, function() {
+    console.log('App is listening on PORT: ' + PORT);
 });
