cf3222ea35bb71f014138915cf6e992c4571670c
Stephen Green <stephengreen1028@gmail.com>
2020-01-18 15:53:07 -0700

    tested server.js to make sure it's working, the message 'hello world' printed out, app is working.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Require in the necessary dependencies 
+var bodyParser = require('body-parser')
 var express = require('express')
 var app = express()
  
@@ -7,3 +8,14 @@ app.get('/', function (req, res) {
 })
  
 app.listen(3000)
+
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+});
