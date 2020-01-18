3483635c2edb3a1b64427c91291dfbfcc324f748
Stephen Green <stephengreen1028@gmail.com>
2020-01-18 15:47:29 -0700

    ran npm init, installed body-parser, express, saved. I also required in the necessary dependencies.

---
+++ server.js
@@ -0,0 +1,9 @@
+// Require in the necessary dependencies 
+var express = require('express')
+var app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
