e49c8c8c2994c9a267b45a1ad7f5533ce28ec511
Stephen Green <stephengreen1028@gmail.com>
2020-01-18 17:03:31 -0700

    Set up table-data.js with an array and then saved that variable to module.exports for later use in another file.

---
+++ app/data/table-data.js
@@ -0,0 +1,14 @@
+// Create table array with information that matches what's on the form
+// ===================================================
+var tableArray = [
+    {
+        customerName: 'Stephen',
+        customerEmail: 'code@example.com',
+        customerID: 'code1234',
+        phoneNumber: '000-000-0000'
+    }
+];
+
+// Use module.exports so this information can be used in another file. Store in a variable for ease of use.
+// ===================================================
+module.exports = tableArray;
