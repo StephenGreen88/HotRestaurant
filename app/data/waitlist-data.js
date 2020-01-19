a3266c0d62f43ace5cdb877a8d61e0899b6c647c
Stephen Green <stephengreen1028@gmail.com>
2020-01-18 17:05:37 -0700

    set up waitinglist-data with an array and set up module.exports for later use.

---
+++ app/data/waitlist-data.js
@@ -0,0 +1,14 @@
+// Create table array with information that matches what's on the form
+// ===================================================
+var waitingArray = [
+    {
+        customerName: 'Saima',
+        customerEmail: 'saima@example.com',
+        customerID: 'saimacool',
+        phoneNumber: '000-000-0000'
+    }
+];
+
+// Use module.exports so this information can be used in another file. Store in a variable for ease of use.
+// ===================================================
+module.exports = waitingArray;
