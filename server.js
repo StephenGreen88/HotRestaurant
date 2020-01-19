37edaeae83ab2a41e505306ac980c050b76cd447
Stephen Green <stephengreen1028@gmail.com>
2020-01-18 17:24:42 -0700

    removed some logic to fix a bug in server.js

--- server.js
+++ server.js
@@ -2,14 +2,12 @@
 // =================================================
 var bodyParser = require('body-parser');
 var express = require('express');
+var path = require('path');
+
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: false
 }))
@@ -18,11 +16,9 @@ app.use(bodyParser.urlencoded({
 // =================================================
 app.use(bodyParser.json())
 
-app.use(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
-});
+// Include route files in the server
+// =================================================
+require('./app/routing/html-routes')(app);
 
 // Listen 
 // =================================================
